6d1ac257f47e2322dd0798fa0c08acbfa7ae0c90
Akua66 <Akaaku2021media01@gmail.com>
2024-03-17 11:14:39 +0900

    update: 一旦飽きた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import rinachanData_ from "@/assets/data/emotion.json";
 const rinachanData: RinachanDataType = rinachanData_;
 
 import Select from '@mui/material/Select';
+import Input from '@mui/material/Input';
 import MenuItem from '@mui/material/MenuItem';
 
 const Page = () => {
@@ -19,6 +20,8 @@ const Page = () => {
   const [currentData, setCurrentData] = useState([]);
 
   const [emotions, setEmotions] = useState<string[]>([]);
+
+  const [intervalTime, setIntervalTime] = useState<number>(5);
 
   const rowArray: number[] = Array.from({ length: 13 }, (_, i) => i);
   const columnArray: number[] = Array.from({ length: 20 }, (_, i) => i);
@@ -32,12 +35,22 @@ const Page = () => {
     setEmotions(Object.keys(rinachanData));
   }, []);
 
+  // sごとにemotionsを切り替え
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentIndex: number = emotions.indexOf(currentEmotion);
+      const nextIndex: number = (currentIndex + 1) % emotions.length;
+      setCurrentEmotion(emotions[nextIndex]);
+    }, intervalTime * 1000);
+    return () => clearInterval(interval);
+  }, [currentEmotion, emotions, intervalTime]);
+
   return (
     <div className={styles["root"]}>
-      still in dev... coming soon! <br />
       <Select
         value={currentEmotion}
         onChange={(event) => setCurrentEmotion(event.target.value)}
+        className={styles["select"]}
       >
         {emotions.map((emotion) => (
           <MenuItem key={emotion} value={emotion}>
@@ -45,26 +58,41 @@ const Page = () => {
           </MenuItem>
         ))}
       </Select>
-      {currentData.length !== 0 && (
-        <div className={styles.container}>
-          {rowArray.map((row, rowIndex) => {
-            const currentRow: number[] = currentData[rowIndex];
-            return (
-              <>
-                {columnArray.map((column, columnIndex) => {
-                  const isPink: boolean = currentRow.includes(column);
-                  return (
-                    <span key={`${row}-${column}`}
-                      className={clsx(styles["cell"], isPink && styles["pink"])}
-                    >
-                    </span>
-                  );
-                })}
-              </>
-            )
-          })}
-        </div>
-      )}
+      <span>
+        <Input type="number" value={intervalTime} onChange={(event) => {
+          const current: number = parseInt(event.target.value);
+          if (current < 1 || event.target.value === "") {
+            setIntervalTime(1);
+            return;
+          }
+          setIntervalTime(parseInt(event.target.value))
+        }}
+          className={styles["input"]}
+        />
+        秒ごとに切り替わります
+      </span>
+      <div className={styles["container-wrapper"]}>
+        {currentData.length !== 0 && (
+          <div className={styles.container}>
+            {rowArray.map((row, rowIndex) => {
+              const currentRow: number[] = currentData[rowIndex];
+              return (
+                <div key={`row-${rowIndex}`} className={styles.row}>
+                  {columnArray.map((column, columnIndex) => {
+                    const isPink: boolean = currentRow.includes(column);
+                    return (
+                      <span
+                        key={`cell-${rowIndex}-${columnIndex}`}
+                        className={clsx(styles.cell, isPink && styles.pink)}
+                      ></span>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
